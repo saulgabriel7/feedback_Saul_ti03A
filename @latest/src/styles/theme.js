2f7ccfdc3db712ef46e789d7bcaa8455d1df54ab
Saul Gabriel <saulgabriel069@gmail.com>
2024-06-09 21:47:06 +0000

    style: posicionamento do card no centro da tela

--- @latest/src/styles/theme.js
+++ @latest/src/styles/theme.js
@@ -1,5 +1,5 @@
 export const theme = {    
-    homebackground: "#131518",
+    homeBackground: "#131518",
     cardgradiente: "radial-gradient(98.96% 98.96% at 50% 0%, #232A34 0%, #181E27 100%)",
     textColor: "#969FAD",
     submitButtonColort: "#FC7614",
