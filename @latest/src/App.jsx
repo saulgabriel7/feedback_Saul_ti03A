969f84a4a1f3cf8ede73723e50afb48c257881aa
Saul Gabriel <saulgabriel069@gmail.com>
2024-06-09 20:47:00 +0000

    feat: instalação e implantação do react router dom e do styled components

--- @latest/src/App.jsx
+++ @latest/src/App.jsx
@@ -1,7 +1,22 @@
-export default function () {
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { ThemeProvider } from "styled-components"
+
+import { Home } from "./pages/Home"
+import { theme } from "./styles/theme.js"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+
+  }
+])
+
+
+export function App() {
   return (
-    <div>
-    
-    </div>
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   )
 }
---
+++ @latest/src/styles/theme.js
@@ -0,0 +1,10 @@
+export const theme = {    
+    homebackground: "#131518",
+    cardgradiente: "radial-gradient(98.96% 98.96% at 50% 0%, #232A34 0%, #181E27 100%)",
+    textColor: "#969FAD",
+    submitButtonColort: "#FC7614",
+    white: "#fff",
+    feedbackButtuonColor: "#262E38",
+    feedbackButtonText: "#7C8798",
+
+}
