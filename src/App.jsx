5e62492cf818d360297ee93ce93d7c18416b0b95
Saul Gabriel <saulgabreil069@gmail.com>
2024-06-10 10:23:46 -0300

    Odeio o Git Pod

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { ThemeProvider } from "styled-components"
+
+import { Home } from "./pages/Home"
+import { theme } from "./styles/theme.js"
+import { GlobalStyle } from "./styles/global.js"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+
+  }
+])
+
+
+export function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+      <GlobalStyle/>
+    </ThemeProvider>
+  )
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,10 @@
+export const theme = {    
+    homeBackground: "#131518",
+    cardgradiente: "radial-gradient(98.96% 98.96% at 50% 0%, #232A34 0%, #181E27 100%)",
+    textColor: "#969FAD",
+    submitButtonColor: "#FC7614",
+    white: "#fff",
+    feedbackButtonColor: "#262E38",
+    feedbackButtonText: "#7C8798",
+
+}
